595b248c5fe1e22657f8ae001b4217d7cc2add09
Oscar Chavez <35067409+oscar-chavez@users.noreply.github.com>
2018-11-12 02:42:49 -0800

    Gives name backwards

---
+++ Name.py
@@ -0,0 +1,5 @@
+for i in range(10):
+    print("GCI is Great")
+print("Hello, my name is Oscar and I'm glad to be here")
+name = input('What is your name?')
+print('Hello,',name,'Pleased to meet you! Did you know that your name backwards is', name[::-1])
